025cec3b4b5c0ceed4da8b067798506df064b036
Harry Dix <harry@harrydix.ch>
2020-02-06 10:53:46 +0100

    style: adjusts layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Octocat.png';
 import './App.css';
 
 const App = () => {
@@ -7,18 +7,11 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Search for Github users:
       </header>
+        <section>
+            input box goes here
+        </section>
     </div>
   );
 }
